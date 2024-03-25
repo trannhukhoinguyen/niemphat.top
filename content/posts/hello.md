b1c9442bc513ef69e731fe35792c513772dc9d10
Miguel <miguel@glodival.vn>
2024-03-26 02:58:05 +0700

    update

--- content/posts/hello.md
+++
@@ -1,9 +0,0 @@
----
-title: hello from nuxt
-description: hello from nuxt
-createdAt: 2021-05-20
-updatedAt: 2021-05-20
-slug: hello-from-nuxt
----
-
-# Hello from nuxt
--- pages/home.vue
+++ pages/home.vue
@@ -1,26 +1,8 @@
 <template>
   <div class="default-layout-wrapper">
-    <div class="top-parts">
-      <div class="menu">
-        <AllMenu />
-      </div>
-      <div class="twelve-causes">
-        <TwelveCauses />
-      </div>
+    <div class="menu">
+      <AllMenu />
     </div>
-    <div class="middle-parts">
-      <div class="sky">
-        <Sky />
-      </div>
-      <div class="">
-
-      </div>
-    </div>
-
-    <div class="bottom-parts">
-      <Ocean />
-    </div>
-
   </div>
 </template>
 <script setup lang="ts">
