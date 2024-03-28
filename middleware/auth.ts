f6c191860182ca19689c9400572811629bfd574a
Miguel <miguel@glodival.vn>
2024-03-29 00:22:14 +0700

    update

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  let path = '/twelve-causes';
+  let path = '/kinh-thu-lang-nghiem/chu-lang-nghiem';
 
   // TODO: check access_token from local storage
   if (localStorage.getItem('apollo:crm.token') !== null) {
