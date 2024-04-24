c5088a5614b4f1a97bd21ac0472f55a1aa32f797
Miguel <miguel@glodival.vn>
2024-04-24 16:43:47 +0700

    update

--- components/Chain/MultiParts/index.vue
+++ components/Chain/MultiParts/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="five-multi-part-wrapper no-living-things" v-if="quantity === 5">
+  <div class="multi-part-wrapper non-sentient-creature" v-if="quantity === 5">
       <div class="orb" title="Đất"></div>
       <div class="orb" title="Nước"></div>
       <div class="orb" title="Gió"></div>
       <div class="orb" title="Lửa"></div>
       <div class="orb" title="Không"></div>
   </div>
-  <div class="seven-multi-part-wrapper living-things" v-if="quantity === 7">
+  <div class="multi-part-wrapper sentient-creature" v-if="quantity === 7">
       <div class="orb" title="Đất"></div>
       <div class="orb" title="Nước"></div>
       <div class="orb" title="Gió"></div>
@@ -28,22 +28,19 @@ const props = defineProps({
 </script>
 
 <style scoped lang="scss">
-.five-human-part-wrapper {
-  margin-top: 250px;
-  margin-left: 800px;
-
-  $colors: (
-          orb1: ( color1: #F09EA7, color2: #F09EA7, color3: #ff1055 ),
-          orb2: ( color1: #F6CA94, color2: #F6CA94, color3: #00f2ff ),
-          orb3: ( color1: #FAFABE, color2: #FAFABE, color3: #00ff77 ),
-          orb4: ( color1: #C1EBC0, color2: #C1EBC0, color3: #ffee00 ),
-          orb5: ( color1: #C7CAFF, color2: #C7CAFF, color3: #ff1055 ),
-  );
-
-  @function getColor($orb, $color) {
-    @return map-get(map-get($colors, $orb), $color);
-  }
-
+$colors: (
+        orb1: ( color1: #F09EA7, color2: #F09EA7, color3: #ff1055 ),
+        orb2: ( color1: #F6CA94, color2: #F6CA94, color3: #00f2ff ),
+        orb3: ( color1: #FAFABE, color2: #FAFABE, color3: #00ff77 ),
+        orb4: ( color1: #C1EBC0, color2: #C1EBC0, color3: #ffee00 ),
+        orb5: ( color1: #C7CAFF, color2: #C7CAFF, color3: #ff1055 ),
+        orb6: ( color1: #CDABEB, color2: #CDABEB, color3: #00f2ff ),
+        orb7: ( color1: #F6C2F3, color2: #F6C2F3, color3: #70369d ),
+);
+@function getColor($orb, $color) {
+  @return map-get(map-get($colors, $orb), $color);
+}
+.multi-part-wrapper {
   body {
     background: #09061b;
     margin: 170px auto;
@@ -55,8 +52,8 @@ const props = defineProps({
   .orb {
     border-radius: 50%;
     position: absolute;
-/*    top: 0;
-    left: 0;*/
+    /*    top: 0;
+        left: 0;*/
     width: 100px;
     height: 100px;
     overflow: hidden;
@@ -82,6 +79,9 @@ const props = defineProps({
     }
   }
 
+}
+
+.non-sentient-creature {
   @for $n from 1 through 5 {
     .orb:nth-child(#{$n}) {
       animation: anim-#{$n} 7s infinite ease-in-out;
@@ -106,62 +106,7 @@ const props = defineProps({
   }
 }
 
-.seven-human-part-wrapper {
-  margin-top: 800px;
-  margin-left: 800px;
-
-  $colors: (
-          orb1: ( color1: #F09EA7, color2: #F09EA7, color3: #ff1055 ),
-          orb2: ( color1: #F6CA94, color2: #F6CA94, color3: #00f2ff ),
-          orb3: ( color1: #FAFABE, color2: #FAFABE, color3: #00ff77 ),
-          orb4: ( color1: #C1EBC0, color2: #C1EBC0, color3: #ffee00 ),
-          orb5: ( color1: #C7CAFF, color2: #C7CAFF, color3: #ff1055 ),
-          orb6: ( color1: #CDABEB, color2: #CDABEB, color3: #00f2ff ),
-          orb7: ( color1: #F6C2F3, color2: #F6C2F3, color3: #70369d ),
-  );
-
-  @function getColor($orb, $color) {
-    @return map-get(map-get($colors, $orb), $color);
-  }
-
-  body {
-    background: #09061b;
-    margin: 170px auto;
-    position: relative;
-    width: 250px;
-    height: 250px;
-  }
-
-  .orb {
-    border-radius: 50%;
-    position: absolute;
-/*    top: 0;
-    left: 0;*/
-    width: 100px;
-    height: 100px;
-    overflow: hidden;
-    mix-blend-mode: lighten;
-    // Try screen
-
-    &::before,
-    &::after {
-      content: '';
-      position: absolute;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-    }
-
-    &::before {
-      // background: #09061b;
-    }
-
-    &::after {
-      mix-blend-mode: color-dodge;
-    }
-  }
-
+.sentient-creature {
   @for $n from 1 through 7 {
     .orb:nth-child(#{$n}) {
       animation: anim-#{$n} 7s infinite ease-in-out;
